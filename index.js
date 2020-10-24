fbd9d7c63773c3faed4d6744497fcb65646af77e
lastuniverse <lastuniverse@mail.ru>
2020-10-24 14:57:32 +0200

    autopush: critical dependencies changed

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ class MsgRouter {
     if (!handlers.length) return null;
     if (!path) path = this.separator;
     const self = this;    
-    const parser = (path === this.separator) ? ({ match: () => ({}) }) : new PathToRegex(path, { separators: this.separator });
+    const parser = (path === this.separator) ? ({ match: () => ({}) }) : new PathToRegex(path, { separators: this.separator, toEnd: false });
     
 
     handlers.forEach(handler=>{
@@ -61,7 +61,12 @@ class MsgRouter {
 
           const nextitem = self.handlers[count+1];
           const cb = nextitem?nextitem.next.bind(this):this.cbEnd;
-    
+
+          const message = { ...this.message };
+          message.targetPath = message.targetPath.slice(path.length);
+          // console.log(message.targetPath, path.length, path, params,parser.regexp);
+      
+
           
         //   if (!params && !parser.match(first)) return cb();
           if (!params) return cb();
@@ -80,9 +85,10 @@ class MsgRouter {
 
           }else{
             if (handler instanceof MsgRouter) {
-              const message = { ...this.message };
-            //   message.targetPath = list.join(self.separator);
-              message.targetPath = message.targetPath.slice(0, path.length);
+            //   const message = { ...this.message };
+            // //   message.targetPath = list.join(self.separator);
+            //   message.targetPath = message.targetPath.slice(0, path.length);
+            //   console.log(message.targetPath);
               
               handler.process(message, cb);
             }else if (typeof handler === 'function'&& handler.length<3) {
@@ -135,9 +141,9 @@ module.exports = exports = MsgRouter;
 
 
 
-// const route_aaa = new MsgRouter();
-// const route_bbb = new MsgRouter();
-// const route_ccc = new MsgRouter();
+const route_aaa = new MsgRouter();
+const route_bbb = new MsgRouter();
+const route_ccc = new MsgRouter();
 
 // route_aaa.use((msg, next) => {
 //   console.log('route_aaa', 0, msg);
@@ -145,28 +151,25 @@ module.exports = exports = MsgRouter;
 // });
 
 
-// route_aaa.use('/aaa/bbb/:action*', route_bbb);
+// route_aaa.use('/aaa/bbb', route_bbb);
 
-// route_aaa.use((msg, next) => {
+// route_aaa.use('/aaa',(msg, next) => {
 //   console.log('route_aaa', 1, msg);
-//   // throw 456;
 //   next();
-
 // });
 
-// route_aaa.use('/aaa/:other*',(msg, next) => {
-//   console.log('route_aaa', 2, msg);
-//   // throw 456;
+// route_aaa.use('/aaa/Aparam',(msg, next) => {
+//   console.log('route_aaa', 1, msg);
 //   next();
-
 // });
+
 
 // route_aaa.use((err, msg, next) => {
 //   console.log('route_aaa', "ERROR", err, msg);
 //   next();
 // });
 
-// /*************************************** */
+// // /*************************************** */
 
 // route_bbb.use((msg, next) => {
 //   console.log('route_bbb', 0, msg);
@@ -176,7 +179,7 @@ module.exports = exports = MsgRouter;
 
 // route_bbb.use('/ccc', route_ccc);
 
-// route_bbb.use(':action*',(msg, next)=>{
+// route_bbb.use(':Bparam*',(msg, next)=>{
 //   console.log("route_bbb", 1, msg);
 //   next();
 // });
